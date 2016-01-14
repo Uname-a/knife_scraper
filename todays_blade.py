877cf582e8e97643e768c8ca85ac4e2a701b4125
Casey Bartlett <caseytb@bu.edu>
2016-01-14 18:43:14 -0500

    Added new "carry" module
    Functions similar to weather (using bot db)
    - accepts setcarry and carry commands
    - prints link to 'todays' carry for triggering nick if no nick provided
    or nickname if a nickname is provided

---
+++ todays_blade.py
@@ -0,0 +1,39 @@
+from sopel import web
+from sopel.module import commands, example, NOLIMIT
+
+@commands('carry')
+@example('.carry fooobarrr')
+def carry(bot, trigger):
+    """.carry show what nickname is carrying today."""
+
+    qnick = trigger.group(2)
+    target_nick = ""
+    if not qnick:
+        carry_url = bot.db.get_nick_value(trigger.nick, 'todayscarry')
+        if not carry_url:
+            return bot.msg(trigger.sender, "I don't know what you're carrying. " +
+                    "Tell my what you're carrying like, .setcarry imgur.com")
+        target_nick = trigger.nick
+    else:
+        target_nick = qnick.strip()
+        carry_url = bot.db.get_nick_value(target_nick, 'todayscarry')
+        if not carry_url:
+            return bot.msg(trigger.sender, "I don't know what {nickname} is carrying. ".format(target_nick) +
+                    "{nickname}: tell my what you're carrying like, .setcarry imgur.com".format(target_nick))
+
+    bot.say("{nick} is currently carrying {url}".format(nick=target_nick, url=carry_url))
+
+@commands('setcarry', 'sc')
+@example('.tc http://imgur.com')
+def update_carry(bot, trigger):
+    """Set the knife you're carrying today."""
+    if not trigger.group(2):
+        bot.reply('Give me an imgur url')
+        return NOLIMIT
+
+    body = web.get(trigger.group(2))
+    if not body:
+        return bot.reply("Invalid url")
+    bot.db.set_nick_value(trigger.nick, 'todayscarry', trigger.group(2))
+    bot.reply("I have your carry set as {url}".format(trigger.group(2)))
+
