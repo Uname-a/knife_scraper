c90689e07d69027d714a328c0ef17411a3790d3f
Casey Bartlett <caseytb@bu.edu>
2016-01-14 23:00:10 -0500

    Two new modules
    - A10 trigger
    - Junglass ( newly commited but has been around for a while )

---
+++ brrttt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# bhq_query.py - module for sopel to query blade head quarters site for knife data
+# 
+# Copyright (c) 2016 Casey Bartlett <caseytb@bu.edu>
+# 
+# See LICENSE for terms of usage, modification and redistribution.
+
+from sopel import  *
+import random
+from itertools import repeat, imap
+
+def rand_cap(c):
+   if random.randint(0, 1):
+       return c.upper()
+   return c.lower()
+
+@module.commands('a10')
+@module.rule('(a).*(10)')
+def knife(bot, trigger):
+   bot.say(formatting.color("bbb"*random.randint(1,3),fg=formatting.colors.RED) +\
+      formatting.color(rand_cap("r")*random.randint(10,24),fg=formatting.colors.WHITE) +\
+      formatting.color("t"*random.randint(4,15),fg=formatting.colors.BLUE))
---
+++ junglas.py
@@ -0,0 +1,24 @@
+# coding=utf8
+"""Junglas module for Sopel"""
+from sopel import module
+import random
+from itertools import repeat, imap
+
+def random_cap(c):
+   if random.randint(0, 1):
+       return c.upper()
+   return c.lower()
+
+
+def random_chars(s):
+   new_s = []
+   for c in s:
+       new_s.extend(imap(random_cap, repeat(c, random.randint(1, 10))))
+   return ''.join(new_s)
+
+#print random_chars('hoonglass')
+
+
+@module.rule('.*((j+u+)|(h+oo+))n+g+l+a+s+')
+def junglas(bot, trigger): 
+    bot.say(random_chars('hoonglass!'))
