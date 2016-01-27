1339dd0f3631b28e13ada876f37eb8726f5bcb4e
Adam Cooperman <coopermanster@gmail.com>
2016-01-27 14:30:42 -0500

    cleaning up decorators

--- social.py
+++ social.py
@@ -5,13 +5,13 @@ from sopel.modules import url
 
 @module.rule('(?<!\w)r[/\s](\w+)')
 def subreddit(bot, trigger): 
-    bot.say(@url.process_urls(bot, trigger, 'http://www.reddit.com/r/' + trigger.group(1)))
+    bot.say(url.process_urls(bot, trigger, 'http://www.reddit.com/r/' + trigger.group(1)))
 
 
 @module.rule('(?<!\w)u[/\s](\w+)')
 def subreddit(bot, trigger): 
-    bot.say(@url.process_urls(bot, trigger, 'http://www.reddit.com/u/' + trigger.group(1)))
+    bot.say(url.process_urls(bot, trigger, 'http://www.reddit.com/u/' + trigger.group(1)))
 
 @module.rule('(?<!\w)W@(\w+)')
 def subreddit(bot, trigger): 
-    bot.say(@url.process_urls(bot, trigger, 'http://www.instagram.com/' + trigger.group(1)))
+    bot.say(url.process_urls(bot, trigger, 'http://www.instagram.com/' + trigger.group(1)))
