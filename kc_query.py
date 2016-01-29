cdd426367e1316c2e27fb488c41ca3c0746c1cd2
Casey Bartlett <caseytb@bu.edu>
2016-01-29 00:42:05 -0500

    Crude price compare
    Crude price compare

--- kc_query.py
+++ kc_query.py
@@ -5,9 +5,11 @@
 # 
 # See LICENSE for terms of usage, modification and redistribution.
 
+
 from sopel import  *
 from ddg import ddg
 from extract_blade_info import query_kc_knife, KnifeFormatter, replaceDictKeys
+
 
 @module.commands('kcknife')
 def knife(bot, trigger):
@@ -27,14 +29,7 @@ def knife(bot, trigger):
         bot.reply('Sorry I didn\'t find anything I could parse, but I did find this {url}'.format(url=url))
     else:
         # need to handle "product type" better ( search again if not knife )
-        if knife["Product Type"] == "Knife":
-            d = replaceDictKeys(BHQNAME_TO_DBNAME,knife)
-            kf = KnifeFormatter()
-            results_string = kf.formattedKnife(d)
-            bot.reply(results_string)
-        else:
-            d = replaceDictKeys(BHQNAME_TO_DBNAME,knife)
-            kf = KnifeFormatter()
-            results_string = kf.formattedKnife(d)
-            bot.reply("I know this isn't a knife, but its what i found:" + results_string)
-
+        d = replaceDictKeys(BHQNAME_TO_DBNAME,knife)
+        kf = KnifeFormatter()
+        results_string = kf.formattedKnife(d)
+        bot.reply(results_string)
