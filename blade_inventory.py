8c0e85e0fff466df725fc06f7a520ce5939bb71f
Casey Bartlett <caseytb@bu.edu>
2016-03-22 22:34:38 -0400

    Added interfaces but didn't implement

--- blade_inventory.py
+++ blade_inventory.py
@@ -36,7 +36,7 @@ def inventory(bot, trigger):
     links = format_links(inventory)
     bot.say("{nick}'s inventory is: {urls}".format(nick=target_nick, urls=links))
 
-@commands('setinventory', 'sinv')
+@commands('setinventory', 'sinv', 'isc')
 @example('.isc http://imgur.com/link_1 .. http://imgur.com/link_n')
 def update_inventory(bot, trigger):
     """Set an inventory of your knives."""
@@ -72,3 +72,37 @@ def update_carry(bot, trigger):
     selected_carry = inventory[int(index)]
     bot.db.set_nick_value(trigger.nick, 'todayscarry', selected_carry)
     bot.reply("I have your carry set as {url} from index {idx}".format(url=trigger.group(2), idx=index))
+
+@commands('append_blade', 'apb')
+@example('.append_blade')
+def add_item(bot, trigger):
+    index = trigger.group(2)
+    if not index:
+        return bot.reply('pleased provide the index of the link you want to set as your current carry')
+    inventory = bot.db.get_nick_value(trigger.nick, 'blade_inventory')
+    if not inventory:
+        return bot.msg(trigger.sender, "I don't have an inventory for you. " +
+                "Tell my what you're carrying like, .setinventory imgur.com/link1 imgur.com/link2")
+    selected_carry = inventory[int(index)]
+    bot.db.set_nick_value(trigger.nick, 'todayscarry', selected_carry)
+    bot.reply("I have your carry set as {url} from index {idx}".format(url=trigger.group(2), idx=index))
+
+
+@commands('remove_blade', 'rmb')
+@example('.remove_blade 2','.remove_blade imgur.com')
+def del_item(bot, trigger):
+    index_or_html = trigger.group(2)
+    inventory = bot.db.get_nick_value(trigger.nick, 'blade_inventory')
+    if not inventory:
+        return bot.msg(trigger.sender, "I don't have an inventory for you. " +
+                "Tell my what you're carrying like, .setinventory imgur.com/link1 imgur.com/link2")
+    if not index_or_html:
+        return bot.reply('Tell me which blade to remove by index, \".remove_blade 2\",' +
+                'or by the url \".remove_blade imgur.com/link\"')
+    if index_or_html.isdigit():
+        index = int(index_or_html)
+
+    selected_carry = inventory[int(index)]
+    bot.db.set_nick_value(trigger.nick, 'todayscarry', selected_carry)
+    bot.reply("I have your carry set as {url} from index {idx}".format(url=trigger.group(2), idx=index))
+
