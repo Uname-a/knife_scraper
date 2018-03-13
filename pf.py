d2946d411ba1085ea315e2ab24d546f64932278a
caseytb <caseytb@bu.edu>
2018-03-13 15:17:38 -0400

    useless

--- pf.py
+++ pf.py
@@ -4,7 +4,7 @@ from sopel import  *
 import random
 from itertools import repeat
 
-@module.rule('.[Bb][Ii][Nn][Gg][Oo].*')
+@module.rule('.pf')
 def bingo(bot, trigger):
-    bot.say('BINGO Time! https://i.imgur.com/fbWAzDh.png')
+    bot.say('REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
 
