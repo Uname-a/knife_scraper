144b2ef5f111bb4aa3995916081a4f2122be7e06
Matt Baird <Uname-a@users.noreply.github.com>
2020-01-23 10:35:03 -0600

    added chance

--- fight.py
+++ fight.py
@@ -6,6 +6,7 @@
 # See LICENSE for terms of usage, modification and redistribution.
 
 from sopel.module import commands, example, NOLIMIT
+from sopel.tools import Identifier
 import random
 
 # Each user has the following associated state information (<<key: default>>)
@@ -20,15 +21,20 @@ import random
 # nsteps = 5
 # spacing = floor(logspace(start,stop,nsteps))
 xlMap = {0:10,
-	1:31
-	2:100
-	3:316
+	1:31,
+	2:100,
+	3:316,
 	4:1000}
 
 fightStrings = [ "Bam boom pow! {source} stabs {target} for {damage} damage!",
  "Krakow! {source} filets {target} for {damage} damage!",
  "Reeee {source} uses loud screech on {target} dealing {damage} damage!",
  "Kaboom! {source} uses their dlc for SD to stab {target} for {damage} damage!"]
+
+MissStrings = [ "Oh no {source} misses {target} and deals no damage"]
+
+CritMissStrings = [ "Oh no {source} is confused and attacked themself for {damage} damage!"]
+
 
 class fighter:
 	def __init__(self, db, nick):
@@ -72,48 +78,97 @@ class fightEvents:
 	# if xl changes:
 	# - write xl to database
 	# - generate message
-	def onXpChange(fighter, gainedXp):
-		newXl = fighter.xl
-		newXp = gainedXp + fighter.xp
+	def onXpChange(aFighter, gainedXp):
+		newXl = aFighter.xl
+		newXp = gainedXp + aFighter.xp
 		while xlMap[newXl] < newXp:
 			newXl += 1
-		xlDiff = newXl - fighter.xl
-		if xlDiff > 0
-			fighter.setXl(newXl)
+		xlDiff = newXl - aFighter.xl
+		if xlDiff > 0:
+			aFighter.setXl(newXl)
 		else:
 			return ""
-		msg = "You gained {totalLevels} experience level".format(xlDiff)
+		msg = "You gained {totalLevels} experience level".format(totalLevels=xlDiff)
 		if xlDiff  > 1:
 			msg += "s"
 		return msg;
-
-def fight(source, target):
+#todo add benifits to levels aka attack chance and armor?
+def fightImpl(source, target):
 	maxIndex = len(fightStrings) - 1
 	minDamage = 1
-	maxDamage = 10
+	maxDamage = 25
 	minXpGain = 2
-	maxXpGain = 15
+	maxXpGain = 30
+	minAttack = 1
+	maxAttack = 100
+	attack = random.randint(minAttack, maxAttack)
 	index = random.randint(0, maxIndex)
 	damage = random.randint(minDamage, maxDamage)
-	baseMsg = fightStrings[index].format(source=source, target=target, damage=damage)
-	damageMsg = target.receiveDamage(damage)
+	damageMsg =""
+	if attack >= 50 and attack < 95:
+		baseMsg = fightStrings[index].format(source=source.nick, target=target.nick, damage=damage)
+		damageMsg = target.receiveDamage(damage)
+	elif attack >= 95:
+		damage = damage * 2
+		baseMsg = fightStrings[index].format(source=source.nick, target=target.nick, damage=damage)
+		damageMsg = target.receiveDamage(damage)
+	elif attack < 50 and attack > 5:
+		baseMsg = fightStrings[index].format(source=source.nick, target=target.nick)
+		damageMsg = target.receiveDamage(damage)
+	elif attack <= 5:
+		damage = damage / 2
+		baseMsg = fightStrings[index].format(source=source.nick, damage=damage)
+		damageMsg = source.receiveDamage(damage)
+	else:
+		baseMsg = fightStrings[index].format(source=source.nick, target=target.nick, damage=damage)
+		damageMsg = target.receiveDamage(damage)
 	xlChangedMessage = ""
+	
 	if damageMsg:
 		# Generate some random xp:
 		xpGained = random.randint(minXpGain, maxXpGain)
-		xlChangedMessage = fighter.receiveExperience(xpGained)
-	return "{base} {damage} {xl}".format(baseMsg, damageMsg, xlChangedMessage)
+		xlChangedMessage = source.receiveExperience(xpGained)
+	return "{base} {damage} {xl}".format(base=baseMsg, damage=damageMsg, xl=xlChangedMessage)
 
 @commands('fight')
-@commands('.fight pf')
+@example('.fight pf')
 def fight(bot, trigger):
 	sourceNick = trigger.nick
-	channel = trigger.channel
-	targetNick = trigger.group(2)
+	channel = trigger.sender
+	if not trigger.group(2):
+		bot.say('fight whosits?')
+		return
+	targetNick = Identifier(trigger.group(2).strip())
+	if targetNick == sourceNick:
+		bot.say('Are u slow son?')
+		return
+	if targetNick == "knifebot":
+		bot.say('knifebot dodges with robotic skill')
+		return
+	if channel not in bot.channels:
+		bot.reply('You can''t fight here!')
+		return
 	if targetNick not in bot.channels[channel].users:
 		bot.reply('{target} is not in this channel [[{channel}]]'.format(target=targetNick, channel=channel))
+		return
 	# load the fighters
 	sourceFighter = fighter(bot.db, sourceNick)
 	targetFighter = fighter(bot.db, targetNick)
-	msg = fight(sourceFighter, targetFighter)
-	sopel.say(msg)
+	msg = fightImpl(sourceFighter, targetFighter)
+	bot.say(msg)
+
+@commands('fstat')
+@example('.fstat fooobarrr')
+def fighterStatus(bot, trigger):
+	if not trigger.group(2):
+		bot.say('fighter status for who?')
+		return
+	targetNick = Identifier(trigger.group(2).strip())
+	hitpoints = bot.db.get_nick_value(targetNick,'hitPoints')
+	xl = bot.db.get_nick_value(targetNick,'xl')
+	if not hitpoints:
+		bot.say('I can''t find stats for {nick}'.format(nick=targetNick))
+		return
+	else:
+		bot.say('{nick} has {hp} hit points / 100 @ XL {xl}'.format(nick=targetNick, hp=hitpoints, xl=xl))
+
