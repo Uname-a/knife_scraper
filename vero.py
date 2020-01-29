a3ca8aa785a3e97bdd7fdfa06e933cbbddcb4359
Matt Baird <Uname-a@users.noreply.github.com>
2020-01-29 15:55:35 -0600

    add stupid stuff

---
+++ vero.py
@@ -0,0 +1,8 @@
+from sopel import  *
+import random
+from itertools import repeat
+
+@module.rule('.[Vv][Ee][Rr][Oo].*')
+def vero(bot, trigger):
+    bot.say('veroengineering.com https://usercontent.irccloud-cdn.com/file/VEpr8xMr/test.png')
+
